41ea1271592a8e4e202df5216b633a395f82de51
vipxkw <47981717+vipxkw@users.noreply.github.com>
2020-12-25 09:08:19 +0800

    Update install-dev.sh

--- install-dev.sh
+++ install-dev.sh
@@ -12,7 +12,7 @@ plain='\033[0m'
 software=(Docker Docker_Caddy Docker_Caddy_cloudflare)
 operation=(install update_config update_image logs)
 # Make sure only root can run our script
-[[ $EUID -ne 0 ]] && echo -e "[${red}Error${plain}] This script must be run as root!" && exit 1
+[[ $EUID -ne 0 ]] && echo -e "[${red}Error${plain}] 此脚本必须以ROOT用户身份运行!" && exit 1
 
 #Check system
 check_sys(){
@@ -97,18 +97,18 @@ get_char(){
 error_detect_depends(){
     local command=$1
     local depend=`echo "${command}" | awk '{print $4}'`
-    echo -e "[${green}Info${plain}] Starting to install package ${depend}"
+    echo -e "[${green}Info${plain}] 开始安装包 ${depend}"
     ${command} > /dev/null 2>&1
     if [ $? -ne 0 ]; then
-        echo -e "[${red}Error${plain}] Failed to install ${red}${depend}${plain}"
-        echo "Please visit: https://teddysun.com/486.html and contact."
+        echo -e "[${red}Error${plain}] 安装失败 ${red}${depend}${plain}"
+        echo "请查看: https://teddysun.com/486.html "
         exit 1
     fi
 }
 
 # Pre-installation settings
 pre_install_docker_compose(){
-    echo "Which Panel Do you use SSpanel 0， SSRpanel 1"
+    echo "您使用哪个面板：SSpanel 0，SSRpanel 1"
     read -p "(v2ray_paneltype (Default 0):" v2ray_paneltype
     [ -z "${v2ray_paneltype}" ] && v2ray_paneltype=0
     echo
@@ -117,7 +117,7 @@ pre_install_docker_compose(){
     echo "---------------------------"
     echo
     # Set sspanel node_id
-    echo "sspanel node_id"
+    echo "sspanel 节点：id"
     read -p "(Default value: 0 ):" sspanel_node_id
     [ -z "${sspanel_node_id}" ] && sspanel_node_id=0
     echo
@@ -136,7 +136,7 @@ pre_install_docker_compose(){
     echo
 
     # Set caddy cloudflare ddns email
-    echo "cloudflare email for tls (optional)"
+    echo "cloudflare账号(可选)"
     read -p "(Default hulisang@test.com):" cloudflare_email
     [ -z "${cloudflare_email}" ]  && cloudflare_email="hulisang@test.com"
     echo
@@ -146,7 +146,7 @@ pre_install_docker_compose(){
     echo
 
     # Set caddy cloudflare ddns key
-    echo "cloudflare key for tls (optional)"
+    echo "cloudflare密钥(可选)"
     read -p "(Default bbbbbbbbbbbbbbbbbb ):" cloudflare_key
     [ -z "${cloudflare_key}" ] && cloudflare_key="bbbbbbbbbbbbbbbbbb"
     echo
@@ -156,7 +156,7 @@ pre_install_docker_compose(){
     echo
     echo
 
-    echo "Which connection do you prefer 0 for webapi 1 for mysql"
+    echo "连接方式 0 webapi 1 for mysql"
     read -p "(v2ray_usemysql (Default 0):" v2ray_usemysql
     [ -z "${v2ray_usemysql}" ] && v2ray_usemysql=0
     echo
@@ -166,7 +166,7 @@ pre_install_docker_compose(){
     echo
 
 
-    echo "Which docker image address will be used"
+    echo "docker映像地址"
     read -p "(image address (Default dnsahvfakcvbpnj/bchdga:1.0):" docker_addresss
     [ -z "${docker_addresss}" ] && docker_addresss="dnsahvfakcvbpnj/bchdga:1.0"
     echo
@@ -177,7 +177,7 @@ pre_install_docker_compose(){
 
 
 
-    echo "Which MUREGEX will be used"
+    echo "请输入（多用户混淆参数表达式，%5m代表取用户特征 md5 的前五位，%id 代表用户id,%suffix 代表上面这个后缀）（可食用：%5m%id.%suffix） MU_REGEX"
     read -p "(MUREGEX (Default %5m%id.%suffix):" MUREGEX
     [ -z "${MUREGEX}" ] && MUREGEX="%5m%id.%suffix"
     echo
@@ -187,7 +187,7 @@ pre_install_docker_compose(){
     echo
 
 
-    echo "Which MUSUFFIX will be used"
+    echo "请输入（多用户混淆参数后缀） MU_SUFFIX:"
     read -p "(MUSUFFIX (Default microsoft.com):" MUSUFFIX
     [ -z "${MUSUFFIX}" ] && MUSUFFIX="microsoft.com"
     echo
@@ -197,7 +197,7 @@ pre_install_docker_compose(){
     echo
 
 
-    echo "Do u use proxy protocol"
+    echo "代理协议"
     read -p "(ProxyTCP (Default 0):" ProxyTCP
     [ -z "${ProxyTCP}" ] && ProxyTCP=0
     echo
@@ -210,8 +210,8 @@ pre_install_docker_compose(){
     if [ "${v2ray_usemysql}" -eq 0 ];
         then
       # Set sspanel_url
-    echo "Please sspanel_url"
-    read -p "(There is no default value please make sure you input the right thing):" sspanel_url
+    echo "SSpanel地址"
+    read -p "(没有默认值，请确保输入的内容正确):" sspanel_url
     [ -z "${sspanel_url}" ]
     echo
     echo "---------------------------"
@@ -219,8 +219,8 @@ pre_install_docker_compose(){
     echo "---------------------------"
     echo
     # Set sspanel key
-    echo "sspanel key"
-    read -p "(There is no default value please make sure you input the right thing):" sspanel_key
+    echo "sspanel面板 key"
+    read -p "(没有默认值，请确保输入的内容正确):" sspanel_key
     [ -z "${sspanel_key}" ]
     echo
     echo "---------------------------"
@@ -230,7 +230,7 @@ pre_install_docker_compose(){
     else
 
    # Set Setting if the node go downwith panel
-    echo "Setting Myqlhost"
+    echo "数据库IP"
     read -p "(v2ray_mysqlhost :" v2ray_mysqlhost
     [ -z "${v2ray_mysqlhost}" ] && v2ray_mysqlhost=""
     echo
@@ -239,7 +239,7 @@ pre_install_docker_compose(){
     echo "---------------------------"
     echo
     # Set Setting if the node go downwith panel
-    echo "Setting MysqlPort"
+    echo "数据库端口"
     read -p "(v2ray_mysqlport (Default 3306):" v2ray_mysqlport
     [ -z "${v2ray_mysqlport}" ] && v2ray_mysqlport=3306
     echo
@@ -248,7 +248,7 @@ pre_install_docker_compose(){
     echo "---------------------------"
     echo
     # Set Setting if the node go downwith panel
-    echo "Setting MysqlUser"
+    echo "数据库用户名"
     read -p "(v2ray_myqluser (Default sspanel):" v2ray_myqluser
     [ -z "${v2ray_myqluser}" ] && v2ray_myqluser="sspanel"
     echo
@@ -257,7 +257,7 @@ pre_install_docker_compose(){
     echo "---------------------------"
     echo
     # Set Setting if the node go downwith panel
-    echo "Setting MysqlPassword"
+    echo "数据库密码"
     read -p "(v2ray_mysqlpassword (Default password):" v2ray_mysqlpassword
     [ -z "${v2ray_mysqlpassword}" ] && v2ray_mysqlpassword=password
     echo
@@ -266,7 +266,7 @@ pre_install_docker_compose(){
     echo "---------------------------"
     echo
     # Set Setting if the node go downwith panel
-    echo "Setting MysqlDbname"
+    echo "数据库名"
     read -p "(v2ray_mysqldbname (Default sspanel):" v2ray_mysqldbname
     [ -z "${v2ray_mysqldbname}" ] && v2ray_mysqldbname=sspanel
     echo
@@ -276,8 +276,8 @@ pre_install_docker_compose(){
     echo
     fi
     # Set sspanel speedtest function
-    echo "use sspanel speedtest"
-    read -p "(sspanel speedtest: Default (6) hours every time):" sspanel_speedtest
+    echo "使用sspanel测试网速"
+    read -p "(sspanel 测速: 默认每(6) 小时1次):" sspanel_speedtest
     [ -z "${sspanel_speedtest}" ] && sspanel_speedtest=6
     echo
     echo "---------------------------"
@@ -286,8 +286,8 @@ pre_install_docker_compose(){
     echo
 
     # Set V2ray backend API Listen port
-    echo "Setting V2ray Grpc API Listen port"
-    read -p "(V2ray Grpc API Listen port(Default 2333):" v2ray_api_port
+    echo "设置V2ray Grpc API侦听端口"
+    read -p "(V2ray Grpc API侦听端口(默认 2333):" v2ray_api_port
     [ -z "${v2ray_api_port}" ] && v2ray_api_port=2333
     echo
     echo "---------------------------"
@@ -296,8 +296,8 @@ pre_install_docker_compose(){
     echo
 
     # Set Setting if the node go downwith panel
-    echo "Setting if the node go downwith panel"
-    read -p "(v2ray_downWithPanel (Default 0):" v2ray_downWithPanel
+    echo "设置节点是否与面板一起关闭"
+    read -p "(v2ray_downWithPanel (默认 0):" v2ray_downWithPanel
     [ -z "${v2ray_downWithPanel}" ] && v2ray_downWithPanel=0
     echo
     echo "---------------------------"
@@ -312,8 +312,8 @@ pre_install_docker_compose(){
 pre_install_caddy(){
 
     # Set caddy v2ray domain
-    echo "caddy v2ray domain"
-    read -p "(There is no default value please make sure you input the right thing):" v2ray_domain
+    echo "caddy v2ray 域名"
+    read -p "(没有默认值，请确保输入的内容正确):" v2ray_domain
     [ -z "${v2ray_domain}" ]
     echo
     echo "---------------------------"
@@ -323,7 +323,7 @@ pre_install_caddy(){
 
 
     # Set caddy v2ray path
-    echo "caddy v2ray path"
+    echo "caddy v2ray 路径"
     read -p "(Default path: /hls/cctv5phd.m3u8):" v2ray_path
     [ -z "${v2ray_path}" ] && v2ray_path="/hls/cctv5phd.m3u8"
     echo
@@ -334,7 +334,7 @@ pre_install_caddy(){
 
     # Set caddy v2ray tls email
     echo "caddy v2ray tls email"
-    read -p "(No default ):" v2ray_email
+    read -p "(没有默认值，请确保输入的内容正确 ):" v2ray_email
     [ -z "${v2ray_email}" ]
     echo
     echo "---------------------------"
@@ -343,7 +343,7 @@ pre_install_caddy(){
     echo
 
     # Set Caddy v2ray listen port
-    echo "caddy v2ray local listen port"
+    echo "caddy v2ray 本地监听端口"
     read -p "(Default port: 10550):" v2ray_local_port
     [ -z "${v2ray_local_port}" ] && v2ray_local_port=10550
     echo
@@ -353,8 +353,8 @@ pre_install_caddy(){
     echo
 
     # Set Caddy  listen port
-    echo "caddy listen port"
-    read -p "(Default port: 443):" caddy_listen_port
+    echo "caddy 监听端口"
+    read -p "(默认: 443):" caddy_listen_port
     [ -z "${caddy_listen_port}" ] && caddy_listen_port=443
     echo
     echo "---------------------------"
@@ -367,12 +367,12 @@ pre_install_caddy(){
 
 # Config docker
 config_docker(){
-    echo "Press any key to start...or Press Ctrl+C to cancel"
+    echo "按任意键开始…或按Ctrl+C取消"
     char=`get_char`
     cd ${cur_dir}
-    echo "install curl"
+    echo "安装 curl"
     install_dependencies
-    echo "Writing docker-compose.yml"
+    echo "创建 docker-compose.yml"
     cat>docker-compose.yml<<EOF
 version: '2'
 
@@ -416,10 +416,10 @@ EOF
 
 # Config caddy_docker
 config_caddy_docker(){
-    echo "Press any key to start...or Press Ctrl+C to cancel"
+    echo "按任意键开始…或按Ctrl+C取消"
     char=`get_char`
     cd ${cur_dir}
-    echo "install curl"
+    echo "安装 curl"
     install_dependencies
     cat>Caddyfile<<EOF
 {\$V2RAY_DOMAIN}:{\$V2RAY_OUTSIDE_PORT}
@@ -439,7 +439,7 @@ config_caddy_docker(){
   realip cloudflare
 }
 EOF
-    echo "Writing docker-compose.yml"
+    echo "创建 docker-compose.yml"
     cat>docker-compose.yml<<EOF
 version: '2'
 
@@ -502,12 +502,12 @@ EOF
 # Config caddy_docker
 config_caddy_docker_cloudflare(){
 
-    echo "Press any key to start...or Press Ctrl+C to cancel"
+    echo "按任意键开始…或按Ctrl+C取消"
     char=`get_char`
     cd ${cur_dir}
-    echo "install curl first "
+    echo "安装 curl first "
     install_dependencies
-    echo "Starting Writing Caddy file and docker-compose.yml"
+    echo "开始写入 Caddy 文件和 docker-compose.yml"
     cat>Caddyfile<<EOF
 {\$V2RAY_DOMAIN}:{\$V2RAY_OUTSIDE_PORT}
 {
@@ -526,7 +526,7 @@ config_caddy_docker_cloudflare(){
   realip cloudflare
 }
 EOF
-    echo "Writing docker-compose.yml"
+    echo "写入 docker-compose.yml"
     cat>docker-compose.yml<<EOF
 version: '2'
 
@@ -589,23 +589,23 @@ EOF
 
 # Install docker and docker compose
 install_docker(){
-    echo -e "Starting installing Docker "
+    echo -e "开始安装 Docker "
     curl -fsSL https://get.docker.com -o get-docker.sh
     bash get-docker.sh
-    echo -e "Starting installing Docker Compose "
+    echo -e "开始安装 Docker Compose "
     curl -L https://github.com/docker/compose/releases/download/1.17.1/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
     chmod +x /usr/local/bin/docker-compose
     curl -L https://raw.githubusercontent.com/docker/compose/1.8.0/contrib/completion/bash/docker-compose > /etc/bash_completion.d/docker-compose
     clear
-    echo "Start Docker "
+    echo "启动 Docker "
     service docker start
-    echo "Start Docker-Compose "
+    echo "启动 Docker-Compose "
     docker-compose pull
     docker-compose up -d
     echo
-    echo -e "Congratulations, V2ray server install completed!"
+    echo -e "恭喜，V2ray服务器安装完成！"
     echo
-    echo "Enjoy it!"
+    echo "好好享受吧！"
     echo
 }
 
@@ -624,33 +624,33 @@ install_select(){
     clear
     while true
     do
-    echo  "Which v2ray Docker you'd select:"
+    echo  "您将选择哪个v2ray Docker："
     for ((i=1;i<=${#software[@]};i++ )); do
         hint="${software[$i-1]}"
         echo -e "${green}${i}${plain}) ${hint}"
     done
-    read -p "Please enter a number (Default ${software[0]}):" selected
+    read -p "请输入一个数字 (默认 ${software[0]}):" selected
     [ -z "${selected}" ] && selected="1"
     case "${selected}" in
         1|2|3|4)
         echo
-        echo "You choose = ${software[${selected}-1]}"
+        echo "你的选择 = ${software[${selected}-1]}"
         echo
         break
         ;;
         *)
-        echo -e "[${red}Error${plain}] Please only enter a number [1-4]"
+        echo -e "[${red}Error${plain}] 请只输入数字 [1-4]"
         ;;
     esac
     done
 }
 install_dependencies(){
     if check_sys packageManager yum; then
-        echo -e "[${green}Info${plain}] Checking the EPEL repository..."
+        echo -e "[${green}Info${plain}] 正在检查EPEL存储库。。。"
         if [ ! -f /etc/yum.repos.d/epel.repo ]; then
             yum install -y epel-release > /dev/null 2>&1
         fi
-        [ ! -f /etc/yum.repos.d/epel.repo ] && echo -e "[${red}Error${plain}] Install EPEL repository failed, please check it." && exit 1
+        [ ! -f /etc/yum.repos.d/epel.repo ] && echo -e "[${red}Error${plain}] 安装EPEL存储库失败，请检查。" && exit 1
         [ ! "$(command -v yum-config-manager)" ] && yum install -y yum-utils > /dev/null 2>&1
         [ x"$(yum-config-manager epel | grep -w enabled | awk '{print $3}')" != x"True" ] && yum-config-manager --enable epel > /dev/null 2>&1
         echo -e "[${green}Info${plain}] Checking the EPEL repository complete..."
@@ -676,25 +676,25 @@ install_dependencies(){
 }
 #update_image
 update_image_v2ray(){
-    echo "Shut down the current service"
+    echo "关闭当前服务"
     docker-compose down
-    echo "Pulling Images"
+    echo "拉取 Images"
     docker-compose pull
-    echo "Start Service"
+    echo "启动服务"
     docker-compose up -d
 }
 
 #show last 100 line log
 
 logs_v2ray(){
-    echo "Last 100 line logs"
+    echo "最后100行日志"
     docker-compose logs --tail 100
 }
 
 # Update config
 update_config_v2ray(){
     cd ${cur_dir}
-    echo "Shut down the current service"
+    echo "关闭当前服务"
     docker-compose down
     install_select
     case "${selected}" in
@@ -713,11 +713,11 @@ update_config_v2ray(){
         config_caddy_docker_cloudflare
         ;;
         *)
-        echo "Wrong number"
+        echo "错误的号码"
         ;;
     esac
 
-    echo "Start Service"
+    echo "启动服务"
     docker-compose pull
     docker-compose up -d
 
@@ -742,7 +742,7 @@ install_v2ray(){
         config_caddy_docker_cloudflare
         ;;
         *)
-        echo "Wrong number"
+        echo "错误的号码"
         ;;
     esac
     install_docker
@@ -752,23 +752,23 @@ install_v2ray(){
 clear
 while true
 do
-echo  "Which operation you'd select:"
+echo  "您将选择哪种操作："
 for ((i=1;i<=${#operation[@]};i++ )); do
     hint="${operation[$i-1]}"
     echo -e "${green}${i}${plain}) ${hint}"
 done
-read -p "Please enter a number (Default ${operation[0]}):" selected
+read -p "请输入一个数字 (默认 ${operation[0]}):" selected
 [ -z "${selected}" ] && selected="1"
 case "${selected}" in
     1|2|3|4)
     echo
-    echo "You choose = ${operation[${selected}-1]}"
+    echo "你的选择 = ${operation[${selected}-1]}"
     echo
     ${operation[${selected}-1]}_v2ray
     break
     ;;
     *)
-    echo -e "[${red}Error${plain}] Please only enter a number [1-4]"
+    echo -e "[${red}Error${plain}] 请只输入数字 [1-4]"
     ;;
 esac
 done
